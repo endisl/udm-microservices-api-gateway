5c7f01be8809c640287c6e60cfc49e997407980a
endisl <endiluamba@gmail.com>
2022-03-01 22:43:04 -0300

    Added SpringDoc to API Gateway and adjusted routes

---
+++ src/main/java/com/example/apigateway/configuration/OpenApiConfiguration.java
@@ -0,0 +1,32 @@
+package com.example.apigateway.configuration;
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SwaggerUiConfigParameters;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+public class OpenApiConfiguration {
+
+    @Bean
+    @Lazy(value = false)
+    public List<GroupedOpenApi> apis(SwaggerUiConfigParameters parameters, RouteDefinitionLocator locator) {
+        var definitions = locator.getRouteDefinitions().collectList().block();
+        definitions.stream().filter(
+                routeDefinition -> routeDefinition.getId().matches(".*-service"))
+                        .forEach(routeDefinition -> {
+                            String name = routeDefinition.getId();
+                            parameters.addGroup(name);
+                            GroupedOpenApi.builder()
+                                    .pathsToMatch("/" + name + "/**")
+                                    .group(name).build();
+                        });
+        return new ArrayList<>();
+    }
+}
--- src/main/java/com/example/apigateway/filter/LoggingFilter.java
+++ src/main/java/com/example/apigateway/filter/LoggingFilter.java
@@ -15,7 +15,7 @@ public class LoggingFilter implements GlobalFilter {
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        logger.info("original request path -> {}", exchange.getRequest().getPath());
+        //logger.info("original request path -> {}", exchange.getRequest().getPath());
         return chain.filter(exchange);
     }
 }
